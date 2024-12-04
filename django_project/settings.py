6e0607fe7e000214790fa4a9f66b259d583e35bc
Javi Huete <fjhuete.m@gmail.com>
2024-12-04 11:04:32 +0000

    Modificaciones examen

--- django_project/settings.py
+++ django_project/settings.py
@@ -69,9 +69,11 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
